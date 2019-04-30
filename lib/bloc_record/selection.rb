77b9b27b4362f40981a1d95b1cd9568021d18482
Ronen Reouveni <ronenreouveni@gmail.com>
2019-04-29 18:24:56 -0700

    4

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -94,7 +94,7 @@ module Selection
       ORDER BY random()
       LIMIT 1;
     SQL
-    
+
     init_object_from_row(row)
   end
 
@@ -130,6 +130,79 @@ def method_missing(method, *args)
   find_by(attribute, args[0])
 end
 
+def where(*args)
+  if args.count > 1
+    expression = args.shift
+    params = args
+  else
+    case args.first
+    when String
+      expression = args.first
+    when Hash
+      expression_hash = BlocRecord::Utility.convert_keys(args.first)
+      expression = expression_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+    end
+  end
+
+  sql = <<-SQL
+    SELECT #{columns.join ","} FROM #{table}
+    WHERE #{expression};
+  SQL
+
+  rows = connection.execute(sql, params)
+  rows_to_array(rows)
+end
+
+def order(*args)
+  arr = []
+  args.each do |arg|
+    case arg
+    when String
+      arr << arg
+    when Symbol
+      arr << arg.to_s
+    when Hash
+      arr << arg.map{|key, value| "#{key} #{value}"}
+    end
+  end
+  order = arr.join(",")
+
+  rows = connection.execute <<-SQL
+    SELECT * FROM #{table}
+    ORDER BY #{order};
+  SQL
+  rows_to_array(rows)
+end
+
+
+def join(*args)
+  if args.count > 1
+    joins = args.map { |arg| "INNER JOIN #{arg} ON #{arg}.#{table}_id = #{table}.id"}.join(" ")
+    rows = connection.execute <<-SQL
+      SELECT * FROM #{table} #{joins}
+    SQL
+  else
+    case args.first
+    when String
+      rows = connection.execute <<-SQL
+        SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(args.first)};
+      SQL
+    when Symbol
+      rows = connection.execute <<-SQL
+        SELECT * FROM #{table}
+        INNER JOIN #{args.first} ON #{args.first}.#{table}_id = #{table}.id
+      SQL
+    when Hash
+      assoc = args.shift
+      rows = connection.execute <<-SQL
+        SELECT * FROM #{table}
+        INNER JOIN #{assoc.first} ON #{assoc.first}.#{table}_id = #{table}.id
+        INNER JOIN #{assoc.last} ON #{assoc.last}.#{assoc.first}_id = #{assoc.first}.id
+      SQL
+    end
+  end
+  rows_to_array(rows)
+end
 
   private
     def init_object_from_row(row)
